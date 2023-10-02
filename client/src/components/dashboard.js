c820344b7edd22ce96a438bc1ad0b58e47bec5d4
SrinivasMM <srinivask@makersmind.in>
2023-10-02 09:15:42 +0530

    dash board link name changed

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -1,19 +1,30 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { Link, useNavigate } from "react-router-dom";
 import "../App.css";
-import React from "react";
+import React, { useContext } from "react";
+import { userContext } from "../App";
 
 function Dashboard() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const {userData} = useContext(userContext)
   return (
     <div className="App">
-      <div>
-        <Link to="/createUsers">Create Users</Link>
-      </div>
+      {userData?.role === "Admin" &&
+        <div>
+          <Link to="/createUsers">Create Users</Link>
+        </div>
+      }
       <div>
         <Link to="/groups">Create Groups</Link>
       </div>
-      <button onClick={(() => {sessionStorage.removeItem('token');navigate('/')})}>logout</button>
+      <button
+        onClick={() => {
+          sessionStorage.removeItem("token");
+          navigate("/");
+        }}
+      >
+        logout
+      </button>
     </div>
   );
 }
